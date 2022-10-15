79d30f3f74a4f779675439f157775fec671210b7
that-ambuj <ambujs89@github.com>
2022-10-15 12:00:11 +0530

    change mongodb_uri to database_url

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -3,9 +3,9 @@ dotenv.config();
 
 export const PORT = process.env.PORT;
 
-export const MONGODB_URI =
+export const DATABASE_URL =
     process.env.NODE_ENV === "test"
-        ? process.env.TEST_MONGODB_URI
-        : process.env.MONGODB_URI;
+        ? process.env.TEST_DATABASE_URL
+        : process.env.DATABASE_URL;
 
 export const NODE_ENV = process.env.NODE_ENV;
