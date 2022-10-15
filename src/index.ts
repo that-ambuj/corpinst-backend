05d2d9f3e09421006c1175711aceb55dcaf785e3
that-ambuj <ambujs89@github.com>
2022-10-15 11:30:23 +0530

    use config in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+import { PORT, NODE_ENV } from "./utils/config";
 
 require("express-async-errors");
 
-const port = process.env.PORT || 3000;
+const port = PORT || 3000;
 
 const app = express();
+
+app.use(cors());
+if (NODE_ENV === "dev") {
+    app.use(morgan("dev"));
+}
 
 app.get("/hello", (req, res) => {
     res.send("Hello World");
