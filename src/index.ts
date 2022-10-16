4674bb6e80584f03f79b0b8a36c34d1a7e304f0f
that-ambuj <ambujs89@github.com>
2022-10-16 12:28:14 +0530

    create assets directory on server start if does not exist

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 import app from "./app";
+import { existsSync, mkdirSync } from "fs";
 import { PORT } from "./utils/config";
 
 const port = PORT || 3000;
+const upload_dir = "assets/public/upload/";
 
 app.listen(port, () => {
+    if (!existsSync(upload_dir)) {
+        mkdirSync(upload_dir, { recursive: true });
+    }
     console.log(`Express is running on port ${port}`);
 });
