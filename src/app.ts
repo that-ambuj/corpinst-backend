67c11458003b7231f35c60dcab460ab3b77601b8
that-ambuj <ambujs89@github.com>
2022-10-16 12:27:55 +0530

    create /upload_file endpoint

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import morgan from "morgan";
+import formidable from "formidable";
 
-import crypto from "crypto";
+import { randomUUID } from "crypto";
 
 import { NODE_ENV } from "./utils/config";
+// TODO add an error handler middleware
 
 // add automatic error handling
 require("express-async-errors");
@@ -29,11 +31,51 @@ app.use(express.static("assets"));
 app.post("/create_new_storage", (req, res) => {
     // TODO check for existing cookies in response
     // TODO add database integration
-    res.cookie("token", crypto.randomUUID());
+    res.cookie("token", randomUUID());
     res.status(200);
-    res.json({
+    return res.json({
         status: "OK",
         message: "Storage Created Successfully",
+    });
+});
+
+app.post("/upload_file", async (req, res, next) => {
+    // TODO add database integration
+    const token = req.cookies.token;
+    if (token === undefined) {
+        res.status(400);
+        return res.json({
+            error: "Please create a storage before uploading file",
+        });
+    }
+
+    const form = formidable({
+        uploadDir: "assets/public/upload/",
+        keepExtensions: true,
+        multiples: false,
+    });
+
+    form.parse(req, (err, fields, { my_file }) => {
+        if (err) {
+            next(err);
+            return;
+        }
+        if (my_file === undefined) {
+            res.status(401);
+            res.json({
+                error: "No file provided",
+            });
+            return;
+        }
+        // todo add new filename to database and link with user token
+        // @ts-ignore wrong type declarations causing issues accessing valid keys
+        const newFileName = my_file.newFilename;
+        res.status(202);
+        res.json({
+            status: "OK",
+            file_path: `public/upload/${newFileName}`,
+        });
+        return;
     });
 });
 
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,8 +1,10 @@
-import { describe, test, expect } from "@jest/globals";
+import { test, expect } from "@jest/globals";
 import supertest from "supertest";
+import { createReadStream, existsSync } from "fs";
+import path from "path";
 import app from "../src/app";
 
-const api = supertest(app);
+const api = supertest.agent(app);
 
 test("Create New Storage", async () => {
     const res = await api.post("/create_new_storage");
@@ -12,4 +14,17 @@ test("Create New Storage", async () => {
         message: "Storage Created Successfully",
     });
     expect(res.header["set-cookie"]).toBeDefined();
+});
+
+test("Upload File", async () => {
+    const file = createReadStream(path.join(__dirname, "test_files/text.txt"));
+    const res = await api
+        .post("/upload_file")
+        .attach("my_file", file)
+        .set("Content-Type", "multipart/form-data");
+
+    expect(res.status).toBe(202);
+    const file_path = path.join("assets", res.body.file_path);
+    expect(existsSync(file_path)).toBe(true);
+    expect(res.header["content-type"]).toMatch("json");
 });
