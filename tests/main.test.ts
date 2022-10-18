709e9f450366af2f81e6900a26ac4283d644fce5
that-ambuj <ambujs89@gmail.com>
2022-10-18 17:02:02 +0530

    safely disconnect mongodb after tests

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,10 +1,18 @@
-import { describe, test, expect } from "@jest/globals";
+import { test, expect, beforeAll, afterAll } from "@jest/globals";
 import supertest from "supertest";
 import { existsSync } from "fs";
 import path from "path";
 import app from "../src/app";
+import Storage from "../src/models/Storage";
+import mongoose from "mongoose";
 
 const api = supertest.agent(app);
+
+const uploaded_files: string[] = [];
+
+beforeAll(async () => {
+    await Storage.deleteMany();
+});
 
 test("Create New Storage", async () => {
     const res = await api.post("/create_new_storage");
@@ -23,45 +31,28 @@ test("Upload File", async () => {
         .attach("my_file", text_file)
         .set("Content-Type", "multipart/form-data");
 
+    uploaded_files.push(res.body.file_path);
+
     expect(res.status).toBe(202);
     const file_path = path.join("assets", res.body.file_path.toString());
     expect(existsSync(file_path)).toBe(true);
     expect(res.header["content-type"]).toMatch("application/json");
 });
 
-describe("Convert text to audio", () => {
-    // add every uploaded file's name to below array
-    const uploaded_files: string[] = [];
-    // get a cookie first
-    api.post("/create_new_storage")
-        .then()
-        .catch((e) => console.error(e));
-    // upload a text file
-    const text_file = path.join(__dirname, "test_files/text.txt");
+test("Should convert text to audio", async () => {
+    const res = await api
+        .post("/text_file_to_audio")
+        .send({ file_path: uploaded_files[0] });
 
-    api.post("/upload_file")
-        .set("Cookie", "token=07ca9df1-6dd2-4413-abbe-6d1bd6a7ccbb")
-        .set("Content-Type", "multipart/form-data")
-        .attach("my_file", text_file)
-        .then((res) => {
-            uploaded_files.push(res.body.file_path);
-        })
-        .catch((err) => {
-            console.error(err);
-        });
-
-    test("Should convert text to audio", async () => {
-        const res = await api
-            .post("/text_file_to_audio")
-            .send({ file_path: uploaded_files[0] })
-            .type("json");
-
-        expect(res.status).toBe(202);
-        expect(res.header["content-type"]).toMatch("application/json");
-        expect(res.body).toMatchObject({
-            status: "OK",
-            message: "Text to Speech Converted",
-        });
-        expect(res.body.audio_file_path).toBeDefined();
+    expect(res.status).toBe(202);
+    expect(res.header["content-type"]).toMatch("application/json");
+    expect(res.body).toMatchObject({
+        status: "OK",
+        message: "Text to Speech Converted",
     });
+    expect(res.body.audio_file_path).toBeDefined();
+});
+
+afterAll(async () => {
+    mongoose.disconnect();
 });
